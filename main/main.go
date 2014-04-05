2fde7ba73f341da7b55191b71f8ba7e4d8497855
Claus Strasburger <claus@strasburger.de>
2014-04-05 20:43:52 +0200

    building a kernel

--- main/main.go
+++ main/main.go
@@ -18,12 +18,18 @@ import (
 )
 
 type Context struct {
-	cl C.cl_context
+	Err       C.cl_int
+	C_context C.cl_context
+
+	NumDevices C.cl_uint
+	DeviceIDs  []C.cl_device_id
 }
 
 type Buffer C.cl_mem
 
 func CreateContext() *Context {
+	c := &Context{}
+
 	// get platform count
 	var platformCount C.cl_uint
 	C.clGetPlatformIDs(0, nil, &platformCount)
@@ -33,14 +39,13 @@ func CreateContext() *Context {
 	C.clGetPlatformIDs(platformCount, &platformIDs[0], nil)
 
 	// get device count
-	var deviceIdCount C.cl_uint
 	C.clGetDeviceIDs(platformIDs[0], C.CL_DEVICE_TYPE_ALL, 0, nil,
-		&deviceIdCount)
-	fmt.Println("Devices:", deviceIdCount)
+		&c.NumDevices)
+	fmt.Println("Devices:", c.NumDevices)
 	// get device IDs
-	deviceIDs := make([]C.cl_device_id, deviceIdCount)
-	C.clGetDeviceIDs(platformIDs[0], C.CL_DEVICE_TYPE_ALL, deviceIdCount,
-		&deviceIDs[0], nil)
+	c.DeviceIDs = make([]C.cl_device_id, c.NumDevices)
+	C.clGetDeviceIDs(platformIDs[0], C.CL_DEVICE_TYPE_ALL, c.NumDevices,
+		&c.DeviceIDs[0], nil)
 
 	// make context
 	contextProps := []C.cl_context_properties{
@@ -49,29 +54,35 @@ func CreateContext() *Context {
 		0,
 	}
 
-	var clErr C.cl_int
-	context := C.clCreateContext(&contextProps[0], 1, &deviceIDs[0], nil, nil,
-		&clErr)
-	if clErr != C.CL_SUCCESS {
-		fmt.Println("CL error", clErr, "!")
-		return nil
-	}
-	return &Context{context}
+	c.C_context = C.clCreateContext(&contextProps[0], 1, &c.DeviceIDs[0], nil, nil,
+		&c.Err)
+	c.CheckErr()
+	return c
 }
 
 func (c *Context) CreateBuffer(data []float32) Buffer {
-	var clErr C.cl_int
 	var buffer C.cl_mem
-	buffer = C.clCreateBuffer(c.cl, C.CL_MEM_READ_ONLY|C.CL_MEM_COPY_HOST_PTR,
+	buffer = C.clCreateBuffer(c.C_context, C.CL_MEM_READ_ONLY|C.CL_MEM_COPY_HOST_PTR,
 		C.size_t(int(unsafe.Sizeof(data[0]))*len(data)), unsafe.Pointer(&data[0]),
-		&clErr)
-
-	if clErr != C.CL_SUCCESS {
-		fmt.Println("CL error", clErr, "!")
-		return nil
-	}
+		&c.Err)
+	c.CheckErr()
 	return Buffer(buffer)
 }
+
+func (c *Context) CheckErr() {
+	if c.Err != C.CL_SUCCESS {
+		fmt.Println("CL error", c.Err, "!")
+		panic("CL error")
+	}
+}
+
+const sourceCode = `
+__kernel void SAXPY (__global float* x, __global float* y, float a)
+{
+    const int i = get_global_id (0);
+
+    y [i] += a * x [i];
+}`
 
 func main() {
 	ctx := CreateContext()
@@ -79,5 +90,12 @@ func main() {
 	x := []float32{1, 2, 3, 4}
 	buffer := ctx.CreateBuffer(x)
 
-	fmt.Println(buffer)
+	str := C.CString(sourceCode)
+	prog := C.clCreateProgramWithSource(ctx.C_context, 1, &str, nil, &ctx.Err)
+	ctx.CheckErr()
+
+	C.clBuildProgram(prog, ctx.NumDevices, &ctx.DeviceIDs[0], nil, nil, nil)
+	ctx.CheckErr()
+
+	fmt.Println(prog, buffer)
 }
